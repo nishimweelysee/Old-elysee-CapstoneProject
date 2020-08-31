0ef27c4461f33dee35afdff4fc41858dc94c833a
nellyganza <nishimwelys@gmail.com>
2020-08-31 08:27:27 +0200

    after adding google loggin option

--- assests/js/Signin.js
+++ assests/js/Signin.js
@@ -12,10 +12,7 @@
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
-
-
 //sign in
-
 
 document.getElementById('signinid').onclick= function() {
     // var chack = document.getElementById('uname').checkValidity() && getElementById('pwd').checkValidity();
@@ -39,3 +36,27 @@ document.getElementById('signinid').onclick= function() {
         }
     })
 }
+
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+document.getElementById('google-signin').onclick =  function (){
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+        console.log(user);
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+        console.log(error.message);
+      });
+};
